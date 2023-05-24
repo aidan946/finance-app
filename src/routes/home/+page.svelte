70f25827243f6f435346133447b842a637cef023
Aidan Stevens <aidan@lookedafter.com>
2023-05-24 19:40:56 +1000

    Add user auth

---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { AppBar } from '@skeletonlabs/skeleton';
+	import { currentUser, pb } from '$lib/pocketbase';
+</script>
+
+{#if $currentUser}
+	<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+		<svelte:fragment slot="lead">(icon)</svelte:fragment>
+		(title)
+		<svelte:fragment slot="trail">(actions)</svelte:fragment>
+	</AppBar>
+	<h1>You are home {$currentUser.email}!</h1>
+{/if}
